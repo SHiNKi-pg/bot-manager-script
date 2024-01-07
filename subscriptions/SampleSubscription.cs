9d4bb4a3fc9d904a47e7bb65d52795c65b73ae91
SHiNKi <shinki.5ktri@gmail.com>
2024-01-07 21:34:06 +0900

    サンプルソース追加

---
+++ subscriptions/SampleSubscription.cs
@@ -0,0 +1,26 @@
+﻿using BotManager.Common.Scripting;
+using BotManager.Common.Scripting.Attributes;
+using BotManager.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bot_manager_script.subscriptions
+{
+    [Action]
+    [Ignore]
+    public class SampleSubscription : ISubscription<SubscriptionArguments>
+    {
+        public IDisposable SubscribeFrom(SubscriptionArguments args)
+        {
+            CompositeDisposable disposables = new();
+
+            // TODO: イベントの購読
+
+            return disposables;
+        }
+    }
+}
